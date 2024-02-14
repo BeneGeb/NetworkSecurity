4d6d8d70df6d30156b3b044736a032ec75044ad4
BeneGeb <benedikt-gebauer@web.de>
2024-02-14 14:10:57 +0100

    update client.p und server.py

--- client.py
+++ client.py
@@ -1,3 +1,43 @@
+from dns import resolver
+import base64
 
-if __name__ == "__main__":
-    print("ich bin ein client")
+def send_dns_query(domain, dns_server):
+    try:
+        r = resolver.Resolver()
+        r.nameservers = [dns_server]
+        r.resolve(domain, 'A', lifetime=1)
+    except:
+        pass
+
+def readTextFile():
+    file_path = './text.txt'
+    with open(file_path, 'r') as file:
+        file_content = file.read()
+    return file_content
+
+def encodeBase64(content):
+    content_bytes = content.encode('utf8')
+    b64_content_bytes = base64.b64encode(content_bytes)
+    return b64_content_bytes
+
+def split_bytes(data, chunk_size):
+    splitted_data = []
+    for i in range(0, len(data), chunk_size):
+        splitted_data.append(data[i:i+chunk_size])
+
+    return splitted_data
+
+
+if __name__ == '__main__':
+    server_addr = "127.0.0.1"
+
+    file_content = readTextFile()
+    b64Content = encodeBase64(file_content)
+
+    splitted_bytes = split_bytes(b64Content, 63)
+
+    for data in splitted_bytes:
+        decoded_string = data.decode('utf-8')
+        send_dns_query(decoded_string, server_addr)
+
+    
--- server.py
+++ server.py
@@ -1,3 +1,34 @@
+import socket, glob, json, struct
+from dnslib import DNSRecord, DNSHeader, QTYPE, RR, A
+import base64
 
-if __name__ == "__main__":
-    print("ich bin ein server")
+
+
+def parse_dns_response(data):
+    dns_request = DNSRecord.parse(data)
+    domain = str(dns_request.q.qname).rstrip('.')
+
+    return domain
+   
+    
+
+if __name__ == '__main__':
+    received_data = ""
+
+    port = 53
+    ip = '127.0.0.1'
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, port))
+
+    while True:
+        data, addr = sock.recvfrom(512)
+        r = parse_dns_response(data)
+
+        received_data += r
+
+        if r[-1] == "=":
+            received_bytes = received_data.encode('utf-8')
+            b64decoded = base64.b64decode(received_bytes)
+            print(b64decoded.decode('utf-8'))
+    
